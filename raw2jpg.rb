7b031ab4b805122849af9d30a622bb5ea61286c0
yukihico <yukihico@gmail.com>
2016-01-02 17:25:05 +0900

    first commit

---
+++ raw2jpg.rb
@@ -0,0 +1,23 @@
+#! /usr/bin/ruby
+
+def raw2jpg(file_or_dir)
+  if File.directory?(file_or_dir)
+    ((Dir.entries file_or_dir) - ['.', '..']).each do |entry|
+        raw2jpg(File.join(file_or_dir, entry))
+    end
+  end
+
+  if raw_file?(file_or_dir)
+    `dcraw -e #{file_or_dir}`
+  end
+end
+
+def raw_file?(path)
+  return unless File.file?(path)
+  raw_exts = %w(.cr2 .crw .raw .dng .erf .raf .kdc .mrw .nef .nrw .orf .rw2 .pef .srw .arw .x3f)
+  raw_exts.include?(File.extname(path).downcase)
+end
+
+if __FILE__ == $0
+  raw2jpg(ARGV[0])
+end
